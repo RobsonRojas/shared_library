ed59c2d27033c5ab0b6b48dc7748ca81a386bf2e
robson.rojas <robson.rojasa@gmail.com>
2022-08-24 22:54:42 -0300

    base commit

---
+++ shared_lib/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "shared_lib.h"
+
+int teste() {
+	printf("teste\n");
+	return 0;
+}
---
+++ use_shared_lib/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "shared_lib.h"
+
+int main(void) {
+	teste();
+	printf("main\n");
+	return 0;
+}
